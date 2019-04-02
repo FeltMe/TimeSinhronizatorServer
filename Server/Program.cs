f06a5a00c426d3311d0517682485af0f70ae503e
sddrozd <sddrozd@A21COMP7>
2019-04-02 20:46:48 +0300

    First

---
+++ Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UdpClient receiver = new UdpClient(8080);
+            IPEndPoint remoteIp = null;
+            Console.WriteLine("Start Server");
+
+            while (true)
+            {
+                byte[] data = receiver.Receive(ref remoteIp);
+                Console.WriteLine(Encoding.Unicode.GetString(data));
+                IPEndPoint ip = remoteIp as IPEndPoint;
+
+                data = Encoding.Unicode.GetBytes("Ok!");
+                Console.WriteLine(data.ToString());
+
+                receiver.Send(data, data.Length, ip.Address.ToString(), ip.Port);
+            }
+        }
+    }
+}
--- Time sinhronizator/Program.cs
+++ Time sinhronizator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace Time_sinhronizator
     {
         static void Main(string[] args)
         {
+            UdpClient sender = new UdpClient();
+            Console.WriteLine("Send message:");
+            byte[] data = Encoding.Unicode.GetBytes(Console.ReadLine());
+
+            sender.Send(data, data.Length, "127.0.0.1", 8080);
+            IPEndPoint remoteIp = null;
+
+            //Прийом даних
+            data = sender.Receive(ref remoteIp);
+
+            Console.WriteLine(Encoding.Unicode.GetString(data));
         }
     }
 }
